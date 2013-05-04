338c515b93735f48c7696259cc7fb3bb04403571
Jacky TSUI <raven02>
2013-05-04 17:13:28 +0800

    Add ffmpeg 1.2 library
    Add Android, IOS build script and static libraries
    
    Should be make a minimal versions of static library..
    
    Remove unnecessary headers
    
    Fix compiling errors on Android
    
    Forgot to remove the comment
    
    Add avconfig.h
    
    Update common.h
    
    Create inttypes.h
    
    Add import library to ppsspp-ffmpeg
    
    Use release 1.2 import library instead of git one
    
    vit9696's mac fixes.
    
    Buildfix for non-Windows. Scripts are executable. Add blackberry build script.
    
    Add  shell script for win32x86 build. use msvc compiler on the MinGW toolchain.
    
    FIX find command did not work.
    CHANGE build-directory. ./x86 -> ./Windows/x86
    ADD commnad. rename *.a -> *.lib and copy inttypes.h in the build-directory.
    ADD variable for switching to shared/static
    
    FIX wrong Path.
    
    Make the android static lib build work again under MingW.
    
    Get rid of libavutil/common.h hack
    
    Strip down Android FFMPEG build to only the parts we need.
    
    Prebuilt Android FFMPEG static libs. This isn't great but needed for now.
    
    Add the generated include files. Again, this is ugly...
    
    Some updates to windows build script
    
    Update to latest git version of ffmpeg. Contains fix for static libraries.
    
    More updates to windows build script
    
    Prebuilt static FFMPEG for Win32. x64 coming later.
    
    Update gitignore
    
    Update README.md
    
    Update Blackberry script. Add headers and libraries for FFMPEG.
    
    add armv7s,create fat libraries
    
    Buildfix: Blackberry
    
    Update Windows and Android FFMPEG to include mpegps, which fixes video support as implemented by the video-support branch.
    
    Add mp3 demuxer. Update build scripts, and Windows binaries.
    
    Update Blackberry FFMPEG.
    
    64-bit FFMPEG build for Windows
    
    Add common error to help people trying to build this.
    
    This solved it just fine for me.
    
    Corrected lipo lines in ios-build.sh (missing .a exts); added iOS binaries
    
    Build ffmpeg for 64-bit MacOSX
    
    Add linux bulid script
    
    Basic low effort copy of mac build script
    
    Add linux prebuilt lib
    
    Add Symbian support.
    
    Automatically copy Symbian libs to LIB dir with correct naming. Symbian doesn't seem to like external LIB dirs. It also doesn't like linking .a files.
    
    Use a symbian-install file for people who just want the libs.
    
    Fix ffmpeg for Symbian. Works perfect now.
    
    Update symbian scripts.
    
    FFMPEG for Android x86
    
    FFMPEG for Linux x86.
    
    Add build script.
    
    Add a snapshot of ffmpeg master
    
    Merge Maxim's Atrac3+ patch
    
    Rebuild FFMPEG for MacOSX-64 with Maxim's Atrac3+ patch (pre-review)
    
    Add atrac3p to android build script
    
    Rebuild FFMPEG with AT3+ support for Linux 64-bit. Add at3+ to blackberry build script.
    
    Rebuild FFMPEG with AT3+ support for Blackberry 10.
    
    Rebuild FFMPEG with AT3+ support for Symbian.
    
    Add new header files (Symbian, BB, Linux) from new ffmpeg drop.
    
    Rebuild FFMPEG for Win64 with AT3+
    
    Rebuild FFMPEG for Win32 with Atrac3+
    
    Rebuild FFMPEG for Android with Atrac3+
    
    Alignment crash fixes. I've sent this patch to Maxim, too.
    
    Rebuilt the Win32 x86 libs with the alignment fix.
    
    Update win64 build with align fix
    
    Revert "Merge Maxim's Atrac3+ patch"
    
    This reverts commit 1d740add74cc5bb937026f8499e8f3905c328bdf.
    
    Conflicts:
    	libavcodec/atrac3plusdsp.c
    
    Apply Maxim's latest Atrac3+ patch
    
    Update build scripts
    
    Update Android and Windows builds with Maxim's latest patch.
    
    Update Linux 64-bit for Maxim's latest change.
    
    Update Symbian for Maxim's latest change.
    
    Update Blackberry for Maxim's latest change.
    
    Rebuild for mac 64-bit
    
    Update FFMPEG-maxim with Linux x86 libraries.
    
    Update iOS libraries, trim some unneeded code.

---
+++ ios-build.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#build ffmpeg for armv7,armv7s and uses lipo to create fat libraries and deletes the originals
+PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/
+
+GENERAL="\
+   --enable-cross-compile \
+   --arch=arm \
+   --cc=$PLATFORM/usr/bin/gcc"
+
+MODULES="\
+   --disable-filters \
+   --disable-programs \
+   --disable-network \
+   --disable-avfilter \
+   --disable-postproc \
+   --disable-encoders \
+   --disable-protocols \
+   --disable-hwaccels \
+   --disable-doc"
+
+VIDEO_DECODERS="\
+   --enable-decoder=h264 \
+   --enable-decoder=mpeg4 \
+   --enable-decoder=mpeg2video \
+   --enable-decoder=mjpeg \
+   --enable-decoder=mjpegb"
+
+AUDIO_DECODERS="\
+    --enable-decoder=aac \
+    --enable-decoder=aac_latm \
+    --enable-decoder=atrac3 \
+    --enable-decoder=atrac3p \
+    --enable-decoder=mp3 \
+    --enable-decoder=pcm_s16le \
+    --enable-decoder=pcm_s8"
+
+DEMUXERS="\
+    --enable-demuxer=h264 \
+    --enable-demuxer=m4v \
+    --enable-demuxer=mpegvideo \
+    --enable-demuxer=mpegps \
+    --enable-demuxer=mp3 \
+    --enable-demuxer=avi \
+    --enable-demuxer=aac \
+    --enable-demuxer=pmp \
+    --enable-demuxer=oma \
+    --enable-demuxer=pcm_s16le \
+    --enable-demuxer=pcm_s8 \
+    --enable-demuxer=wav"
+
+PARSERS="\
+    --enable-parser=h264 \
+    --enable-parser=mpeg4video \
+    --enable-parser=mpegaudio \
+    --enable-parser=mpegvideo \
+    --enable-parser=aac \
+    --enable-parser=aac_latm"
+
+./configure \
+    --prefix=ios/armv7 \
+    $GENERAL \
+    --sysroot="$PLATFORM/SDKs/iPhoneOS6.1.sdk" \
+    --extra-cflags="-arch armv7 -mfpu=neon -miphoneos-version-min=6.0" \
+    --disable-shared \
+    --enable-static \
+    --extra-ldflags="-arch armv7 -isysroot $PLATFORM/SDKs/iPhoneOS6.1.sdk -miphoneos-version-min=6.0" \
+    --enable-zlib \
+    --disable-everything \
+    ${MODULES} \
+    ${VIDEO_DECODERS} \
+    ${AUDIO_DECODERS} \
+    ${DEMUXERS} \
+    ${PARSERS} \
+    --target-os=darwin \
+    --enable-vfp \
+    --enable-neon \
+    --cpu=cortex-a8 \
+    --enable-pic
+
+make clean
+make && make install
+
+if [ "$?" != "0" ]; then
+    exit 1;
+fi
+
+./configure \
+    --prefix=ios/armv7s \
+    $GENERAL \
+    --sysroot="$PLATFORM/SDKs/iPhoneOS6.1.sdk" \
+    --extra-cflags="-arch armv7s -mfpu=neon -miphoneos-version-min=6.0" \
+    --disable-shared \
+    --enable-static \
+    --extra-ldflags="-arch armv7s -isysroot $PLATFORM/SDKs/iPhoneOS6.1.sdk -miphoneos-version-min=6.0" \
+    --enable-zlib \
+    --disable-everything \
+    ${MODULES} \
+    ${VIDEO_DECODERS} \
+    ${AUDIO_DECODERS} \
+    ${DEMUXERS} \
+    ${PARSERS} \
+    --target-os=darwin \
+    --enable-vfp \
+    --enable-neon \
+    --cpu=cortex-a9 \
+    --enable-pic
+
+make clean
+make && make install
+
+if [ "$?" != "0" ]; then
+    exit 1;
+fi
+
+cd ios
+mkdir -p universal/lib
+
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libavformat.a -arch armv7s armv7s/lib/libavformat.a -output universal/lib/libavformat.a
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libavutil.a -arch armv7s armv7s/lib/libavutil.a -output universal/lib/libavutil.a
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libswresample.a -arch armv7s armv7s/lib/libswresample.a -output universal/lib/libswresample.a
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libavcodec.a -arch armv7s armv7s/lib/libavcodec.a -output universal/lib/libavcodec.a
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libswscale.a -arch armv7s armv7s/lib/libswscale.a -output universal/lib/libswscale.a
+
+xcrun -sdk iphoneos lipo -create -arch armv7 armv7/lib/libavdevice.a -arch armv7s armv7s/lib/libavdevice.a -output universal/lib/libavdevice.a
+
+cp -r armv7/include universal/
+
+rm -rf armv7 armv7s
