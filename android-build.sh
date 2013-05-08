bf50bff78a6d4f47da12496e2c9714f5801d2691
Henrik Rydgard <hrydgard@gmail.com>
2013-05-08 17:40:55 +0200

    Strip down Android FFMPEG build to only the parts we need.

--- android-build.sh
+++ android-build.sh
@@ -4,31 +4,74 @@ NDK=/c/AndroidNDK
 PLATFORM=$NDK/platforms/android-8/arch-arm/
 PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/windows-x86_64
 
+GENERAL="\
+   --enable-cross-compile \
+   --extra-libs="-lgcc" \
+   --arch=arm \
+   --cc=$PREBUILT/bin/arm-linux-androideabi-gcc \
+   --cross-prefix=$PREBUILT/bin/arm-linux-androideabi- \
+   --nm=$PREBUILT/bin/arm-linux-androideabi-nm"
 
+MODULES="\
+   --disable-filters \
+   --disable-programs \
+   --disable-network \
+   --disable-avfilter \
+   --disable-postproc \
+   --disable-encoders \
+   --disable-doc \
+   --disable-ffplay \
+   --disable-ffprobe \
+   --disable-ffserver \
+   --disable-ffmpeg"
+
+VIDEO_DECODERS="\
+   --enable-decoder=h264 \
+   --enable-decoder=mpeg2video"
+
+AUDIO_DECODERS="\
+    --enable-decoder=aac \
+    --enable-decoder=atrac1 \
+    --enable-decoder=atrac3 \
+    --enable-decoder=mp3 \
+    --enable-decoder=pcm_s16le \
+    --enable-decoder=pcm_s8"
+  
+DEMUXERS="\
+    --enable-demuxer=h264 \
+    --enable-demuxer=mpegvideo \
+    --enable-demuxer=avi \
+    --enable-demuxer=aac \
+    --enable-demuxer=oma \
+    --enable-demuxer=pcm_s16le \
+    --enable-demuxer=pcm_s8 \
+    --enable-demuxer=wav"
+
+PARSERS="\
+    --enable-parser=h264 \
+    --enable-parser=mpeg4video \
+    --enable-parser=mpegvideo \
+    --enable-parser=aac \
+    --enable-parser=mpegaudio" 
 
 
 function build_ARMv6
 {
 ./configure --target-os=linux \
     --prefix=./android/armv6 \
-    --enable-cross-compile \
-    --extra-libs="-lgcc" \
-    --arch=arm \
-    --cc=$PREBUILT/bin/arm-linux-androideabi-gcc \
-    --cross-prefix=$PREBUILT/bin/arm-linux-androideabi- \
-    --nm=$PREBUILT/bin/arm-linux-androideabi-nm \
+    ${GENERAL} \
     --sysroot=$PLATFORM \
     --extra-cflags=" -O3 -fpic -DANDROID -DHAVE_SYS_UIO_H=1 -Dipv6mr_interface=ipv6mr_ifindex -fasm -Wno-psabi -fno-short-enums -fno-strict-aliasing -finline-limit=300 -DCMP_HAVE_VFP -mfloat-abi=softfp -mfpu=vfp -marm -march=armv6" \
     --disable-shared \
     --enable-static \
     --extra-ldflags="-Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib -lc -lm -ldl -llog" \
     --enable-zlib \
-    --disable-filters \
-    --disable-encoders \
-    --disable-ffplay \
-    --disable-ffprobe \
-    --disable-ffserver \
-    --disable-ffmpeg \
+    --disable-everything \
+    ${MODULES} \
+    ${VIDEO_DECODERS} \
+    ${AUDIO_DECODERS} \
+    ${DEMUXERS} \
+    ${PARSERS} \
     --disable-neon
 
 make clean
@@ -39,29 +82,24 @@ function build_ARMv7
 {
 ./configure --target-os=linux \
     --prefix=./android/armv7 \
-    --enable-cross-compile \
-    --extra-libs="-lgcc" \
-    --arch=arm \
-    --cc=$PREBUILT/bin/arm-linux-androideabi-gcc \
-    --cross-prefix=$PREBUILT/bin/arm-linux-androideabi- \
-    --nm=$PREBUILT/bin/arm-linux-androideabi-nm \
+    ${GENERAL} \
     --sysroot=$PLATFORM \
     --extra-cflags=" -O3 -fpic -DANDROID -DHAVE_SYS_UIO_H=1 -Dipv6mr_interface=ipv6mr_ifindex -fasm -Wno-psabi -fno-short-enums -fno-strict-aliasing -finline-limit=300 -mfloat-abi=softfp -mfpu=vfp -marm -march=armv7-a" \
     --disable-shared \
     --enable-static \
     --extra-ldflags="-Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib -lc -lm -ldl -llog" \
     --enable-zlib \
-    --disable-avfilter \
-    --disable-encoders \
-    --disable-postproc \
-    --disable-ffplay \
-    --disable-ffprobe \
-    --disable-ffserver \
-    --disable-ffmpeg \
+    --disable-everything \
+    ${MODULES} \
+    ${VIDEO_DECODERS} \
+    ${AUDIO_DECODERS} \
+    ${DEMUXERS} \
+    ${PARSERS} \
     --disable-neon \
 
 make clean
 make install
 }
-#build_ARMv6
+
+build_ARMv6
 build_ARMv7
